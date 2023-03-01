74fb93e1ffbccc2f1dce5d0b87b1ceddc7040cb5
Joel Dacuma <joeldacuma1@gmail.com>
2023-03-01 21:45:51 +0800

    build version

---
+++ out/_next/static/JaNsBkesIDVlF3OOEH7JA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-edf96e97197812f2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/[...all]":["static/chunks/pages/[...all]-9704a58000a686b9.js"],sortedPages:["/","/_app","/_error","/[...all]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
